92f0b8ac5709e697897c596496a90cd74159b235
iluxa327git <135642994+iluxa327git@users.noreply.github.com>
2024-01-30 16:01:36 +0300

    Update Map with constructors and getter/setters

--- src/entity/Map.java
+++ src/entity/Map.java
@@ -1,6 +1,31 @@
 package entity;
 
 public class Map {
-    private int[][] map = new int[16][16];
-    private Ship[] ships = new Ship[11];
+    private int[][] map;
+    private Ship[] ships;
+
+    public Map(int[][] map, Ship[] ships) {
+        this.map = map;
+        this.ships = ships;
+    }
+
+    public Map() {
+        this.map = new int[16][16];
+        this.ships = new Ship[21];
+    }
+    public int[][] getMap() {
+        return map;
+    }
+
+    public void setMap(int[][] map) {
+        this.map = map;
+    }
+
+    public Ship[] getShips() {
+        return ships;
+    }
+
+    public void setShips(Ship[] ships) {
+        this.ships = ships;
+    }
 }
